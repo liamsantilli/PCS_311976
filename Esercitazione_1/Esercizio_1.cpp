e5ec4f6cfec043acb98095c817386896e076f83b
liamsantilli <liamsantilli@gmail.com>
2026-03-18 20:10:08 +0000

    Consegna esercitazione 1

--- Esercitazione_1/Esercizio_1.cpp
+++ Esercitazione_1/Esercizio_1.cpp
@@ -1,13 +1,31 @@
 #include <fstream>
+#include <iostream>
+#include <string>
+
 using namespace std;
-int main(void) {
-string filename = "input_es1.txt";
-ifstream ifs(filename);
-if ( ifs.is_open() ) { // Check if file successfully opened
-while( !ifs.eof() ) {
-string location;
-double temp;
-ifs >> location >> temp; // also >> : S×T →S
-cout << "Temperature at " << location << " is " << temp << "\n";
-}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Errore: specificare il file di input!" << endl; // FIX: aggiunto messaggio
+        return 1;
+    }
+
+    string filename = argv[1];
+    ifstream ifs(filename);
+
+    if (ifs.is_open()) {
+        string location;
+        double t1, t2, t3, t4;
+
+        while (ifs >> location >> t1 >> t2 >> t3 >> t4) {
+            double media = (t1 + t2 + t3 + t4) / 4.0;
+            cout << location << " " << media << "\n";
+        }
+        ifs.close();
+    } else {
+        cerr << "Errore: impossibile aprire il file " << filename << "\n";
+        return 1;
+    }
+
+    return 0;
 }
